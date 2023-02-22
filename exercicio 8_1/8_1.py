0e0486ab6a77a9240f075026edcf8b28f42f355c
Fantiko <86378890+Fantiko@users.noreply.github.com>
2023-02-22 01:31:57 -0300

    estudos

---
+++ exercicio 8_1/8_1.py
@@ -0,0 +1,23 @@
+#8.5 Open the file mbox-short.txt and read it line by line.
+# When you find a line that starts with 'From ' like the following line:
+#From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+#You will parse the From line using split() and print out the second word in the line
+# (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+#Hint: make sure not to include the lines that start with 'From:'.
+# Also look at the last line of the sample output to see how to print the count.
+
+fh = open('mbox-short.txt')
+contador = 0
+
+for line in fh:
+    line = line.rstrip()
+    wc = line.split()
+    #Guardiao
+    if len(wc) < 3:
+        continue
+    if wc[0] != 'From':
+        continue
+    print(wc[1])
+    contador = contador + 1
+print("There were", contador, "lines in the file with From as the first word")
+
